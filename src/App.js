d44434b63379e9b890a5c6cead0199d82a610363
nagata03 <nagata.job@gmail.com>
2020-02-02 00:29:23 +0900

    learn class component and functional component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 function App() {
   return (
-    <React.Fragment>
-	  <label htmlFor="bar">bar</label>
-	  <input type="text" onChange={() => {console.log("I am clicked!")}} />
-    </React.Fragment>
+	<div>
+	  <Cat />
+	  <Cat />
+	  <Cat />
+	  <Cat />
+	</div>
   )
+}
+
+function Cat() {
+  return <div>Meow!</div>
 }
 
 export default App;
